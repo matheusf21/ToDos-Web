1cbc42ca9c78023e53e7db542578a518f7bac165
matheusf21 <matheusferreiradelima21@gmail.com>
2026-02-24 11:37:23 -0300

    V0.2

--- ToDoPlatform/Models/ToDo.cs
+++ ToDoPlatform/Models/ToDo.cs
@@ -5,24 +5,29 @@ using ToDoPlatform.Models;
 namespace ToDoPlatform.MOdels;
 
 [Table("todos")]
-public class ToDoPlatform
+public class ToDo
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id {get; set;}
+    public int Id { get; set; }
 
     [Required]
-    public string UserId {get; set;}
-    [ForeignKey(nameof(UserId))]
-    public AppUser User { get; set;}
+    public string UserId { get; set; }
+
+    [ForeignKey("UserId")]
+    [Display(Name = "Usuário")]
+    public AppUser User { get; set; }
 
     [StringLength(100)]
     [Display(Name = "Título")]
     public string Title { get; set; }
 
+    [Display(Name = "Descrição")]
     public string Description { get; set; }
 
+    [Display(Name = "Concluído")]
     public bool Done { get; set; } = false;
 
+    [Display(Name = "Data de cadastro")]
     public DateTime CreateAt { get; set; } = DateTime.Now;
 }
