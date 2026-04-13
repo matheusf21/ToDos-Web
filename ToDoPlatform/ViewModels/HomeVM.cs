f636266391f269adb5155e071ca719b9256b0d10
matheusf21 <matheusferreiradelima21@gmail.com>
2026-04-13 11:37:33 -0300

    V0.9

---
+++ ToDoPlatform/ViewModels/HomeVM.cs
@@ -0,0 +1,9 @@
+using ToDoPlatform.Models;
+namespace ToDoPlatform.ViewModels;
+public class HomeVM
+{
+public int TotalTasks { get; set; }
+public int OpenTasks { get; set; }
+public int EndedTasks { get; set; }
+public List<ToDo> ToDos { get; set; }
+}
